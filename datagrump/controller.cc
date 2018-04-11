b4a03cab2486f6538d06887cc2d21df1924358a2
Reynis Vazquez-Guzman <reynis@stanford.edu>
2018-04-11 01:24:36 -0700

    remove max window size since no longer necessary

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -10,7 +10,6 @@ using namespace std;
 const unsigned int ADD_INCREASE = 1;
 const float MULT_DECREASE = .5;
 
-const float MAX_WINDOW_SZ = 200;
 float the_window_size = 1;
 
 /* Default constructor */
@@ -21,16 +20,13 @@ Controller::Controller( const bool debug )
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size()
 {
-  unsigned int effective_window_size = floor(min(the_window_size,MAX_WINDOW_SZ));
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
-	 << " window size is " << the_window_size
-   << ", effective window size is " << effective_window_size
-   << endl;
+	 << " window size is " << the_window_size << endl;
   }
 
-  return effective_window_size;
+  return the_window_size;
 }
 
 /* A datagram was sent */
@@ -42,6 +38,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 				    /* datagram was sent because of a timeout */ )
 {
   /* AIMD: decrease window size if datagram was sent due to timeout */
+  /* check if window size is greater than 1 to avoid underflow */
   if (after_timeout && the_window_size > 1) {
     the_window_size *= MULT_DECREASE;
   }
@@ -66,9 +63,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
      (duplicate acks are not possible since sender never
      sends same sequence number twice and
      receiver only acks received packets once)*/
-  if (the_window_size < MAX_WINDOW_SZ) {
-    the_window_size += ADD_INCREASE / the_window_size;
-  }
+  the_window_size += ADD_INCREASE / the_window_size;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
@@ -83,5 +78,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms()
 {
-  return 25;
+  return 50;
 }
